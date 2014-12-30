2c2ffb8db61e7b1a9c50b72d6b79c4fed2741329
niehao <niehao100@gmail.com>
2014-12-31 00:11:22 +0800

    写完文档

--- senseros.c
+++ senseros.c
@@ -17,10 +17,10 @@ void GPIOA6INT(void){
 	for(t_s=0;t_s<10000&&(GPIOPinRead(GPIO_PORTA_BASE,GPIO_PIN_5)||GPIOPinRead(GPIO_PORTA_BASE,GPIO_PIN_7));t_s++);
 
 
-		if(((senser_flag%2)==0)&&t_s>450)	{for(i=0;i<150;i++) move_stright(-4);if((abs(abs(center_local[0]-128)-(128-_Px))<40)&&(abs(abs(center_local[1]-128)-(128-_Py))<40)) S_flag--;}
-		if(((senser_flag%2)==1)&&t_s>310)	{for(i=0;i<150;i++) move_stright(4);if((abs(abs(center_local[0]-128)-(128-_Px))<40)&&(abs(abs(center_local[1]-128)-(128-_Py))<40)) S_flag--;}/*
-		if((senser_flag%3)==2)	{for(i=0;i<50;i++) turn_left_back(); S_flag=1;}*/
-		if(S_flag<-1000) S_flag=-10;
+	if(((senser_flag%2)==0)&&t_s>450)	{for(i=0;i<150;i++) move_stright(-4);if((abs(abs(center_local[0]-128)-(128-_Px))<40)&&(abs(abs(center_local[1]-128)-(128-_Py))<40)) S_flag--;}
+	if(((senser_flag%2)==1)&&t_s>310)	{for(i=0;i<150;i++) move_stright(4);if((abs(abs(center_local[0]-128)-(128-_Px))<40)&&(abs(abs(center_local[1]-128)-(128-_Py))<40)) S_flag--;}/*
+																																													 if((senser_flag%3)==2)	{for(i=0;i<50;i++) turn_left_back(); S_flag=1;}*/
+	if(S_flag<-1000) S_flag=-10;
 
 	IntEnable(INT_GPIOA);
 
